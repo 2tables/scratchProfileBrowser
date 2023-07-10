a4fec3adc2f002d0066e0c69088320ecbf1c81aa
Zach Nomad <137446166+2tables@users.noreply.github.com>
2023-07-09 19:54:19 -0500

    function i didn't steal

--- cookies.js
+++ cookies.js
@@ -1,2 +1,16 @@
 document.cookie = "recent=2tables";
 console.log("cookies: " + document.cookie);
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
