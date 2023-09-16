6a66a899ce1b83dc837b236b8bfe22073637b2f9
Zach Nomad <137446166+2tables@users.noreply.github.com>
2023-09-16 09:26:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import scratchattach as scratch3
 import sys
 
-username = input("UserName: ")
+username = input("Username: ")
 password = input("Password: ")
 try:
   session = scratch3.login(username, password) # use password --> call for imports
 except Exception as e:
     print("There was a " + type(e).__name__ + " error during login")
-    sys.exit(1)
+    sys.exit(2)
 
 if (session.banned):
     print("Unfortunately, you has been banned from Scratch\n. See you next time!")
