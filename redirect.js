d386962b391e904d5eeb5814a42907f3272c3e9b
ZNomad <137446166+2tables@users.noreply.github.com>
2024-01-13 08:31:22 -0600

    Delete redirect.js

--- redirect.js
+++
@@ -1,4 +0,0 @@
-const input = document.getElementById("UsernameInput")
-function redirect(value){
-    window.location.href = window.location.href.split("pages/leaderboard.html")[0] + "index.html?u=" + value;
-}
