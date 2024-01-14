e7fb724848a4fa992206fd8d11cdf7797de9715e
ZNomad <137446166+2tables@users.noreply.github.com>
2024-01-13 20:12:22 -0600

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -5,18 +5,17 @@ async function getPage(page){
     for(i = 0; i < data.length; i++){
         user = await document.createElement("span");
         user.className = await "user";
-        user.innerHTML = await `<span><img src="https://cdn2.scratch.mit.edu/get_image/user/${data[i].id}_90x90.png">#${(i + 1) + (page * 100)} - ${data[i].username}</span>${data[i].statistics.followers}`;
+        user.innerHTML = await `<span><img src="https://cdn2.scratch.mit.edu/get_image/user/${data[i].id}_90x90.png">#${(i + 1) + (page * 100)} - ${data[i].username}</span>${data[i].statistics.followers.toLocaleString('en-US')}`;
         document.getElementById("users").appendChild(user);
     }
 }
 index = 0;
 getPage(index);
 
-window.addEventListener('scroll',()=>{
-    if(window.scrollY + window.innerHeight >= 
-    document.documentElement.scrollHeight){
-        console.log("updating")
-    index++;
-    getPage(index);
+window.onscroll = function(){
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+        console.log("updating");
+        index++;
+        getPage(index);
     }
-})
+};
