82dd45540d6fcd83a142ebf74235767af080419f
ZNomad <137446166+2tables@users.noreply.github.com>
2024-01-14 18:24:37 -0600

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,19 +1,23 @@
+users = []
 async function getPage(page){
+    for(i = 0; i < 100; i++){
+        users[i] = await document.createElement("span")
+        users[i].className = await "user";
+        users[i].innerHTML = await `<span><img src="../assets/not_found.png">#${(i + 1) + (page * 100)} - user</span>0`;
+        document.getElementById("users").appendChild(users[i]);
+    }
     res = await fetch(`https://scratchdb.lefty.one/v3/user/rank/global/followers/${page}`);
     data = await res.json()
     console.log(data);
     for(i = 0; i < data.length; i++){
-        user = await document.createElement("span");
-        user.className = await "user";
-        user.innerHTML = await `<span><img src="https://cdn2.scratch.mit.edu/get_image/user/${data[i].id}_90x90.png">#${(i + 1) + (page * 100)} - ${data[i].username}</span>${data[i].statistics.followers.toLocaleString('en-US')}`;
-        document.getElementById("users").appendChild(user);
+        users[i].innerHTML = await `<span><img src="https://cdn2.scratch.mit.edu/get_image/user/${data[i].id}_90x90.png">#${(i + 1) + (page * 100)} - ${data[i].username}</span>${data[i].statistics.followers.toLocaleString('en-US')}`;
     }
 }
 index = 0;
 getPage(index);
 
 window.onscroll = function(){
-    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+    if(window.scrollY + window.innerHeight >= document.body.scrollHeight){
         console.log("updating");
         index++;
         getPage(index);
